d03d9ae0877b00c73e2e19f5ebb410705df43303
schneefux <schneefux+commit@schneefux.xyz>
2019-02-02 18:22:52 +0100

    Update meta title and description

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,14 +1,12 @@
-const pkg = require('./package');
-
 module.exports = {
   mode: 'spa',
 
   head: {
-    title: pkg.name,
+    title: 'Brawl Stars Time Ninja',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: 'Brawl Stars Time Ninja - Check time spent on Brawl Stars and player statistics' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
