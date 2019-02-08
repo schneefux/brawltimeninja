bf093dce5f5cc0639c87b3ce719d37fee4533f76
schneefux <schneefux+commit@schneefux.xyz>
2019-02-08 22:17:31 +0100

    Remove API cache

--- functions/routes/brawlstars.ts
+++ functions/routes/brawlstars.ts
@@ -1,26 +1,11 @@
 import cors from 'cors';
 import express from 'express';
 import BrawlStars from 'brawlstars';
-import cacheManager from 'cache-manager';
-import fsStore from 'cache-manager-fs-hash';
 import { Hero, PlayerStatistic, Mode, Player } from '../Player';
 
-const cacheSeconds = 60*5;
-const cachePath = process.env.CACHE_PATH || 'cache';
-const cacheDisable = !!process.env.CACHE_DISABLE;
 const token = process.env.BRAWLSTARS_TOKEN;
 if (token == undefined) throw new Error('Please set $BRAWLSTARS_TOKEN!');
 
-const cache = cacheDisable ?
-  cacheManager.caching({
-    store: 'memory',
-    max: 0,
-    ttl: cacheSeconds
-  }) :
-  cacheManager.caching(<any>{
-    store: fsStore,
-    options: { path: cachePath, subdirs: true, },
-  });
 const client = new BrawlStars.Client({ token });
 const router = express.Router();
 
@@ -42,132 +27,130 @@ router.get('/featured-players', cors(), async (req, res) => {
 router.get('/player/:tag', cors(), async (req, res, next) => {
   Promise.resolve().then(async function() {
     const tag = req.params.tag;
-    const data = await cache.wrap(`brawlstars-player-${tag}`, async () => {
-      const player = await client.getPlayer(tag);
+    const player = await client.getPlayer(tag);
 
-      const heroes = {} as { [id: string]: Hero };
-      player.brawlers.forEach((brawler) => {
-        const brawlerId = brawler.name.toLowerCase().replace(' ', '_');
-        heroes[brawlerId] = {
-          label: brawler.name,
-          icon: `/images/brawlstars/heroes/icon/${brawlerId}.png`,
+    const heroes = {} as { [id: string]: Hero };
+    player.brawlers.forEach((brawler) => {
+      const brawlerId = brawler.name.toLowerCase().replace(' ', '_');
+      heroes[brawlerId] = {
+        label: brawler.name,
+        icon: `/images/brawlstars/heroes/icon/${brawlerId}.png`,
+        stats: {
+          trophies: {
+            label: 'Current Trophies',
+            value: brawler.trophies,
+            icon: '/images/brawlstars/icon/trophy.png'
+          },
+          maxTrophies: {
+            label: 'Max Trophies',
+            value: brawler.highestTrophies,
+            icon: '/images/brawlstars/icon/trophy.png'
+          },
+          level: {
+            label: 'Power Level',
+            value: brawler.level,
+            icon: brawler.level == 10?
+              '/images/brawlstars/icon/starpower.png'
+              :'/images/brawlstars/icon/powerpoint.png'
+          }
+        }
+      } as Hero;
+    });
+
+    const stats = {
+      trophies: {
+        label: 'Current Trophies',
+        value: player.trophies
+      },
+      highestTrophies: {
+        label: 'Max Trophies',
+        value: player.highestTrophies
+      },
+      expLevel: {
+        label: 'Experience Level',
+        value: player.expLevel
+      },
+    } as { [id: string]: PlayerStatistic };
+
+    if (player.club != undefined) {
+      stats.clubName = {
+        label: 'Club',
+        value: player.club.name
+      };
+    }
+
+    const minutesSpent =
+      player.victories * 2 * 3 + // 50% win rate * 3mins
+      player.soloShowdownVictories * 10 * 3 + // 1/10 win rate * 3mins
+      player.duoShowdownVictories * 5 * 3 // 1/5 win rate * 3mins
+    ;
+
+    const data = {
+      id: player.tag,
+      name: player.name,
+      minutesSpent,
+      heroes,
+      stats,
+      modes: {
+        '3v3': {
+          label: '3v3',
+          icon: '/images/brawlstars/mode/icon/gemgrab.png',
+          background: '/images/brawlstars/mode/background/gemgrab.png',
           stats: {
-            trophies: {
-              label: 'Current Trophies',
-              value: brawler.trophies,
-              icon: '/images/brawlstars/icon/trophy.png'
-            },
-            maxTrophies: {
-              label: 'Max Trophies',
-              value: brawler.highestTrophies,
-              icon: '/images/brawlstars/icon/trophy.png'
-            },
-            level: {
-              label: 'Power Level',
-              value: brawler.level,
-              icon: brawler.level == 10?
-                '/images/brawlstars/icon/starpower.png'
-                :'/images/brawlstars/icon/powerpoint.png'
+            victories: {
+              label: 'Victories',
+              value: player.victories,
             }
           }
-        } as Hero;
-      });
-
-      const stats = {
-        trophies: {
-          label: 'Current Trophies',
-          value: player.trophies
-        },
-        highestTrophies: {
-          label: 'Max Trophies',
-          value: player.highestTrophies
-        },
-        expLevel: {
-          label: 'Experience Level',
-          value: player.expLevel
-        },
-      } as { [id: string]: PlayerStatistic };
-
-      if (player.club != undefined) {
-        stats.clubName = {
-          label: 'Club',
-          value: player.club.name
-        };
-      }
-
-      const minutesSpent =
-        player.victories * 2 * 3 + // 50% win rate * 3mins
-        player.soloShowdownVictories * 10 * 3 + // 1/10 win rate * 3mins
-        player.duoShowdownVictories * 5 * 3 // 1/5 win rate * 3mins
-      ;
-
-      return {
-        id: player.tag,
-        name: player.name,
-        minutesSpent,
-        heroes,
-        stats,
-        modes: {
-          '3v3': {
-            label: '3v3',
-            icon: '/images/brawlstars/mode/icon/gemgrab.png',
-            background: '/images/brawlstars/mode/background/gemgrab.png',
-            stats: {
-              victories: {
-                label: 'Victories',
-                value: player.victories,
-              }
+        } as Mode,
+        'soloShowdown': {
+          label: 'Solo Showdown',
+          icon: '/images/brawlstars/mode/icon/showdown.png',
+          background: '/images/brawlstars/mode/background/showdown.png',
+          stats: {
+            victories: {
+              label: 'Victories',
+              value: player.soloShowdownVictories,
             }
-          } as Mode,
-          'soloShowdown': {
-            label: 'Solo Showdown',
-            icon: '/images/brawlstars/mode/icon/showdown.png',
-            background: '/images/brawlstars/mode/background/showdown.png',
-            stats: {
-              victories: {
-                label: 'Victories',
-                value: player.soloShowdownVictories,
-              }
+          }
+        } as Mode,
+        'duoShowdown': {
+          label: 'Duo Showdown',
+          icon: '/images/brawlstars/mode/icon/duoshowdown.png',
+          background: '/images/brawlstars/mode/background/showdown.png',
+          stats: {
+            victories: {
+              label: 'Victories',
+              value: player.duoShowdownVictories,
             }
-          } as Mode,
-          'duoShowdown': {
-            label: 'Duo Showdown',
-            icon: '/images/brawlstars/mode/icon/duoshowdown.png',
-            background: '/images/brawlstars/mode/background/showdown.png',
-            stats: {
-              victories: {
-                label: 'Victories',
-                value: player.duoShowdownVictories,
-              }
+          }
+        } as Mode,
+        'bossfight': {
+          label: 'Bossfight',
+          icon: '/images/brawlstars/mode/icon/bossfight.png',
+          background: '/images/brawlstars/mode/background/bossfight.png',
+          stats: {
+            minutes: {
+              label: 'survived',
+              value: player.bestTimeAsBoss,
             }
-          } as Mode,
-          'bossfight': {
-            label: 'Bossfight',
-            icon: '/images/brawlstars/mode/icon/bossfight.png',
-            background: '/images/brawlstars/mode/background/bossfight.png',
-            stats: {
-              minutes: {
-                label: 'survived',
-                value: player.bestTimeAsBoss,
-              }
+          }
+        } as Mode,
+        'roborumble': {
+          label: 'Robo Rumble',
+          icon: '/images/brawlstars/mode/icon/roborumble.png',
+          background: '/images/brawlstars/mode/background/roborumble.png',
+          stats: {
+            minutes: {
+              label: 'survived',
+              value: player.bestRoboRumbleTime,
             }
-          } as Mode,
-          'roborumble': {
-            label: 'Robo Rumble',
-            icon: '/images/brawlstars/mode/icon/roborumble.png',
-            background: '/images/brawlstars/mode/background/roborumble.png',
-            stats: {
-              minutes: {
-                label: 'survived',
-                value: player.bestRoboRumbleTime,
-              }
-            }
-          } as Mode,
-        },
-      } as Player;
-    }, { ttl: cacheSeconds });
+          }
+        } as Mode,
+      },
+    } as Player;
 
-    res.set('Cache-Control', `public, max-age=${cacheSeconds}`);
+    res.set('Cache-Control', 'public, max-age=300');
     res.json(data);
   }).catch((error) => {
     if (error == 'Error: Invalid Tag.') {
--- functions/routes/vainglory.ts
+++ functions/routes/vainglory.ts
@@ -1,26 +1,11 @@
 import cors from 'cors';
 import express from 'express';
 import Vainglory from 'vainglory';
-import cacheManager from 'cache-manager';
-import fsStore from 'cache-manager-fs-hash';
 import { Hero, PlayerStatistic, Mode, Player } from '../Player';
 
-const cacheSeconds = 60*5;
-const cachePath = process.env.CACHE_PATH || 'cache';
-const cacheDisable = !!process.env.CACHE_DISABLE;
 const token = process.env.VAINGLORY_TOKEN;
 if (token == undefined) throw new Error('Please set $VAINGLORY_TOKEN!');
 
-const cache = cacheDisable ?
-  cacheManager.caching({
-    store: 'memory',
-    max: 0,
-    ttl: cacheSeconds
-  }) :
-  cacheManager.caching(<any>{
-    store: fsStore,
-    options: { path: cachePath, subdirs: true, },
-  });
 const client = new Vainglory(token, { region: 'eu' });
 const router = express.Router();
 
@@ -36,55 +21,53 @@ router.get('/featured-players', cors(), async (req, res) => {
 router.get('/player/:name', cors(), async (req, res, next) => {
   Promise.resolve().then(async function() {
     const name = req.params.name;
-    const data = await cache.wrap(`vainglory-player-${name}`, async () => {
-      const players = await client.players.getByName([name]);
-      const player = players.data[0];
+    const players = await client.players.getByName([name]);
+    const player = players.data[0];
 
-      const heroes = {} as { [id: string]: Hero };
+    const heroes = {} as { [id: string]: Hero };
 
-      const stats = {
-        skillTier: {
-          label: 'Skill Tier',
-          value: player.attributes.stats.skillTier
-        },
-      } as { [id: string]: PlayerStatistic };
+    const stats = {
+      skillTier: {
+        label: 'Skill Tier',
+        value: player.attributes.stats.skillTier
+      },
+    } as { [id: string]: PlayerStatistic };
 
-      const minutesSpent = 1;
+    const minutesSpent = 1;
 
-      return {
-        id: player.id,
-        name: player.attributes.name,
-        minutesSpent,
-        heroes,
-        stats,
-        modes: {
-          casual: {
-            label: '3v3 Casual',
-            icon: '',
-            background: '',
-            stats: {
-              played: {
-                label: 'Games',
-                value: player.attributes.stats.gamesPlayed.casual,
-              }
+    const data = {
+      id: player.id,
+      name: player.attributes.name,
+      minutesSpent,
+      heroes,
+      stats,
+      modes: {
+        casual: {
+          label: '3v3 Casual',
+          icon: '',
+          background: '',
+          stats: {
+            played: {
+              label: 'Games',
+              value: player.attributes.stats.gamesPlayed.casual,
             }
-          } as Mode,
-          ranked: {
-            label: '3v3 Ranked',
-            icon: '',
-            background: '',
-            stats: {
-              played: {
-                label: 'Games',
-                value: player.attributes.stats.gamesPlayed.ranked,
-              }
+          }
+        } as Mode,
+        ranked: {
+          label: '3v3 Ranked',
+          icon: '',
+          background: '',
+          stats: {
+            played: {
+              label: 'Games',
+              value: player.attributes.stats.gamesPlayed.ranked,
             }
-          } as Mode,
-        },
-      } as Player;
-    }, { ttl: cacheSeconds });
+          }
+        } as Mode,
+      },
+    } as Player;
 
-    res.set('Cache-Control', `public, max-age=${cacheSeconds}`);
+    res.set('Cache-Control', 'public, max-age=300');
     res.json(data);
   }).catch((error) => {
     if (error.response != undefined) {
