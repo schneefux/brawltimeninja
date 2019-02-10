529f38fc22f0baf1be1b18141073ea2458d41852
schneefux <schneefux+commit@schneefux.xyz>
2019-02-10 16:54:25 +0100

    Replace koa response caching by http client response caching

--- functions/app.ts
+++ functions/app.ts
@@ -2,14 +2,9 @@ import Koa from 'koa';
 import cors from '@koa/cors';
 import Router from 'koa-router';
 import compress from 'koa-compress';
-import Cache from 'koa-file-cache';
 import statusRouter from './routes/status';
 import brawlstarsRouter from './routes/brawlstars';
 import vaingloryRouter from './routes/vainglory';
-
-const cacheSeconds = parseInt(process.env.CACHE_SECONDS || '600');
-const cachePath = process.env.CACHE_PATH || 'cache';
-const cacheEnable = process.env.CACHE_DISABLE == undefined;
 
 export default function createApp(path: string = '/api') {
   const app = new Koa();
@@ -20,9 +15,6 @@ export default function createApp(path: string = '/api') {
   router.use('/vainglory' + path, vaingloryRouter.routes());
 
   app.use(cors());
-  if (cacheEnable) {
-    app.use(Cache({ folder: cachePath, cacheTime: cacheSeconds*1000 }));
-  }
   app.use(compress());
   app.use(router.routes());
   return app;
--- functions/util.ts
+++ functions/util.ts
@@ -1,6 +1,23 @@
 import fetch from 'node-fetch';
 import { URLSearchParams, URL } from 'url';
 import { Agent } from 'https';
+import cacheManager from 'cache-manager';
+import fsStore from 'cache-manager-fs-hash';
+
+const cacheSeconds = 60*5;
+const cachePath = process.env.CACHE_PATH || 'cache';
+const cacheDisable = !!process.env.CACHE_DISABLE;
+
+const cache = cacheDisable ?
+  cacheManager.caching({
+    store: 'memory',
+    max: 0,
+    ttl: cacheSeconds
+  }) :
+  cacheManager.caching(<any>{
+    store: fsStore,
+    options: { path: cachePath, subdirs: true, },
+  });
 
 const agent = new Agent({
   keepAlive: true,
@@ -15,10 +32,12 @@ export function request(path: string,
   const url = new URL(path, base);
   const urlParams = new URLSearchParams(params);
   url.search = urlParams.toString();
+  const urlStr = url.toString();
 
-  return fetch(url.toString(), {
-    headers,
-    agent,
-    compress: true,
-  }).then((response) => response.json());
+  return cache.wrap(urlStr, () => fetch(urlStr, {
+      headers,
+      agent,
+      compress: true,
+    }).then((response) => response.json())
+  );
 }
