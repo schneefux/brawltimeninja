f32eae5d18f60903745fc76a0dbbca7bc52ef542
schneefux <schneefux+commit@schneefux.xyz>
2019-02-14 20:11:11 +0100

    Update title: Brawl Stars Time -> Online Time

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,11 +2,11 @@ module.exports = {
   mode: 'spa',
 
   head: {
-    title: 'Brawl Stars Time Ninja',
+    title: 'Online Time Ninja',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Brawl Stars Time Ninja - Check time spent on Brawl Stars and player statistics' }
+      { hid: 'description', name: 'description', content: 'Online Time Ninja - Check time spent on Brawl Stars and player statistics' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
