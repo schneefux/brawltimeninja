81ca3af2e312b5cdac66a9488c31938cabcaabf1
schneefux <schneefux+commit@schneefux.xyz>
2019-02-24 11:52:39 +0100

    API: Add functional tests for AppServices

---
+++ functions/services/__tests__/AppService.test.ts
@@ -0,0 +1,17 @@
+process.env.CACHE_DISABLE = '1';
+
+import createService, { Service } from "../AppServiceFactory";
+
+Object.keys(Service).forEach((serviceName) => {
+  describe(`Test service ${serviceName}`, () => {
+    const service = createService(Service[<keyof typeof Service>serviceName]);
+
+    const featuredPlayers = service.getFeaturedPlayers();
+    featuredPlayers.forEach((featuredPlayer) => {
+      it(`should return data for featured player ${featuredPlayer.name}`, async () => {
+        const data = await service.getPlayerStatistics(featuredPlayer.shard, featuredPlayer.id);
+        expect(data).not.toBeNull();
+      });
+    });
+  });
+});
