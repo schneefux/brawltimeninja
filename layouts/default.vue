754f182c5905945846b7f46ccdfdefc60de0e06c
schneefux <schneefux+commit@schneefux.xyz>
2019-04-07 11:29:38 +0200

    Attempt to fix background flash

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="min-h-screen bg-primary-darkest text-grey-lighter flex flex-col justify-between bg-center bg-cover"
+    class="flex flex-col justify-between min-h-screen bg-primary-darkest text-grey-lighter bg-center"
     :style="`background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('${background}'`">
     <nav class="bg-primary-dark p-6">
       <nuxt-link to="/" class="no-underline font-semibold text-xl text-white tracking-tight">
@@ -11,9 +11,7 @@
     <nuxt />
 
     <footer class="bg-primary-darker py-2 text-sm text-center leading-normal">
-      <p>
-        &#169; 2019 Online Time Ninja Project
-      </p>
+      <p>&#169; 2019 Online Time Ninja Project</p>
       <p class="text-xs leading-tight">{{ labels.disclaimer }}</p>
       <p class="text-xs leading-tight">Send questions or feedback to dev (at) {{ domain }}.</p>
     </footer>
