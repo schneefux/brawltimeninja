4dd12762126a45fe38a59dcc0197aecfc5402109
schneefux <schneefux+commit@schneefux.xyz>
2019-04-25 17:17:56 +0200

    Add an error page

---
+++ layouts/error.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container text-center mx-auto py-4 px-2">
+    <img class="h-32 mt-6" src="/images/hero/model/spike_loss_optimized.png">
+    <h1>Oops!</h1>
+    <h2>{{ error.message }}.</h2>
+    <p class="mt-4">
+      <nuxt-link
+        to="/"
+        class="no-underline text-secondary border-primary border-b-2"
+      >Return to home</nuxt-link>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'default',
+  props: ['error'],
+}
+</script>
