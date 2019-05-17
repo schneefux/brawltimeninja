4f191c4aa8796f15cae3b6b73543eb66141bc4d8
schneefux <schneefux+commit@schneefux.xyz>
2019-05-17 18:03:01 +0200

    Fix recents links by wiping store lastPlayers

--- plugins/persist.js
+++ plugins/persist.js
@@ -4,7 +4,7 @@ export default ({ store }) => {
   window.onNuxtReady(() => new VuexPersistence({
     key: `brawlstars-ninja`,
     reducer: state => ({
-      version: 2,
+      version: 3,
       lastPlayers: state.lastPlayers,
     }),
     restoreState: (key, storage) => {
@@ -12,17 +12,9 @@ export default ({ store }) => {
       let value = (storage).getItem(key)
       value = typeof value === 'string' ? JSON.parse(value || '{}') : (value || {})
 
-      if (value.version === undefined || value.version === 1) {
-        if (value.lastPlayers !== undefined) {
-          // 0 -> 1: rename player.id -> player.tag
-          // 1 -> 2: it broke for some reason so just do it again
-          value.lastPlayers.forEach((player) => {
-            if (player.id !== undefined) {
-              player.tag = player.id
-              delete player.id
-            }
-          })
-        }
+      if (value.version === undefined || value.version < 3) {
+        // player schema change was messed up…
+        value.lastPlayers = []
       }
 
       return value
