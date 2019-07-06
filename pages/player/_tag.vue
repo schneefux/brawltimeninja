09bbb5a64032ff93fd2c65ac7cab72d0a4a8f2e2
schneefux <schneefux+commit@schneefux.xyz>
2019-07-06 21:33:10 +0200

    Battle log: Increase contrast

--- pages/player/_tag.vue
+++ pages/player/_tag.vue
@@ -236,7 +236,7 @@
         >
           <div
             class="h-full items-center card bg-center bg-cover flex flex-wrap justify-between"
-            :style="'background-image: linear-gradient(135deg, rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.25)), url(\'' + require(`~/assets/images/mode/background/${battle.mode.background}`) + '\')'"
+            :style="'background-image: linear-gradient(135deg, rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.5)), url(\'' + require(`~/assets/images/mode/background/${battle.mode.background}`) + '\')'"
           >
             <div class="card-content relative">
               <span class="card-header text-white">
