d8894ae18d315b184ae4ac855cd76c60ee62449a
schneefux <schneefux+commit@schneefux.xyz>
2019-07-06 21:42:09 +0200

    Battle log: Reduce margin between players to prevent layout breaks on xs

--- pages/player/_tag.vue
+++ pages/player/_tag.vue
@@ -266,7 +266,7 @@
                     class="flex items-center mb-2"
                   >
                     <div
-                      class="mx-2 flex items-center rounded-r"
+                      class="mx-1 flex items-center rounded-r"
                       style="background: rgba(0, 0, 0, 0.5)"
                     >
                       <img
