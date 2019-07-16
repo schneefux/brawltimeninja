98985e1e739de559a538bc2d5361a4347f011505
schneefux <schneefux+commit@schneefux.xyz>
2019-07-16 12:26:34 +0200

    Map Meta: Show all maps

--- pages/meta/map/index.vue
+++ pages/meta/map/index.vue
@@ -4,32 +4,100 @@
       <h1 class="mx-2 text-4xl md:text-center mt-2 font-semibold">
         Map Meta
       </h1>
-      <p class="mx-2 text-xl md:text-center mt-2">
-        Select a Map
+      <p class="mx-2 flex flex-wrap md:justify-center md:mx-auto mt-3">
+        <span class="text-xl w-full md:text-center">
+          Table of Contents
+        </span>
+        <ul class="mx-3 mt-2 border-l px-2">
+          <li>
+            <a href="#current">
+              Live right now
+            </a>
+          </li>
+          <li class="mt-2">
+            <a href="#upcoming">
+              Up next
+            </a>
+          </li>
+          <li
+            v-for="(_, mode) in events"
+            :key="mode"
+            class="mt-2"
+          >
+            <a :href="`#${mode}`">
+              {{ formatMode(mode) }}
+            </a>
+          </li>
+        </ul>
       </p>
 
       <div class="mt-6">
-        <h2 class="mx-2 text-xl md:text-center font-semibold">
-          Live right now
-        </h2>
-        <div class="flex flex-wrap justify-center">
-          <event-card
-            v-for="event in currentEvents"
-            :key="event.id"
-            :event="event"
-          />
-        </div>
+        <details
+          id="current"
+          open
+        >
+          <summary class="mx-2 text-xl md:text-center font-semibold">
+            Live right now
+          </summary>
+          <div class="flex flex-wrap justify-center">
+            <event-card
+              v-for="event in currentEvents"
+              :key="event.id"
+              :event="event"
+            />
+          </div>
+        </details>
 
-        <h2 class="mx-2 text-xl md:text-center font-semibold">
-          Up next
+        <details
+          id="upcoming"
+          class="mt-6"
+        >
+          <summary
+            class="mx-2 text-xl md:text-center font-semibold"
+            @click="$set(load, 'upcoming', true)"
+          >
+            Up next
+          </summary>
+          <div
+            v-if="load['upcoming']"
+            class="flex flex-wrap justify-center"
+          >
+            <event-card
+              v-for="event in upcomingEvents"
+              :key="event.id"
+              :event="event"
+            />
+          </div>
+        </details>
+
+        <h2 class="mt-6 mx-2 text-xl md:text-center font-semibold">
+          All maps
         </h2>
-        <div class="flex flex-wrap justify-center">
-          <event-card
-            v-for="event in upcomingEvents"
-            :key="event.id"
-            :event="event"
-          />
-        </div>
+        <details
+          v-for="(modeEvents, mode) in events"
+          :id="mode"
+          :key="mode"
+          class="mt-4"
+        >
+          <summary
+            class="mx-2 md:text-center text-lg font-semibold capitalize"
+            @click="$set(load, mode, true)"
+          >
+            <span class="w-48 inline-block text-left">
+              {{ formatMode(mode) }}
+            </span>
+          </summary>
+          <div
+            v-if="load[mode]"
+            class="flex flex-wrap justify-center"
+          >
+            <event-card
+              v-for="event in modeEvents"
+              :key="event.id"
+              :event="event"
+            />
+          </div>
+        </details>
       </div>
     </div>
   </div>
@@ -39,10 +107,25 @@
 import { mapState, mapActions } from 'vuex'
 import EventCard from '~/components/event-card.vue'
 
+function formatMode(mode) {
+  const camelToSnakeCase = str => str.replace(/[A-Z]/g, letter => `_${letter.toLowerCase()}`)
+  const capitalize = str => str.replace(/(?:^|\s)\S/g, a => a.toUpperCase())
+  return camelToSnakeCase(mode)
+    .split('_')
+    .map(w => capitalize(w))
+    .join(' ')
+}
+
 export default {
   name: 'MetaSelectMapPage',
   components: {
     EventCard,
+  },
+  data() {
+    return {
+      load: {},
+      formatMode,
+    }
   },
   head() {
     return {
@@ -51,16 +134,20 @@ export default {
   },
   computed: {
     events() {
-      // TODO add endpoint for all maps
-      // get a list of events with data for every brawler
-      const commonEventIds = this.meta.map(entry => [...Object.keys(entry.events)])
-        .reduce((acc, cur, index) => index === 0 ? cur : cur.filter(m => acc.includes(m)), [])
-        .sort()
-      return commonEventIds.reduce((events, id) => events.concat([{
-        id: id,
-        mode: this.meta[0].events[id].mode,
-        map: this.meta[0].events[id].map,
-      }]), [])
+      return this.meta.map(entry => [...Object.entries(entry.events)])
+        .reduce((entries, entryEntries) => entries.concat(entryEntries), [])
+        .reduce((events, [id, event]) => events.filter(e => id === e.id).length === 0
+          ? events.concat([{
+            id: id,
+            mode: event.mode,
+            map: event.map,
+          }])
+          : events, [])
+        .sort((e1, e2) => e1.map > e2.map ? 1 : e1.map < e2.map ? -1 : 0)
+        .reduce((eventsByMode, event) => ({
+          ...eventsByMode,
+          [event.mode]: (eventsByMode[event.mode] || []).concat([event]),
+        }), {})
     },
     ...mapState({
       currentEvents: state => state.currentEvents,
