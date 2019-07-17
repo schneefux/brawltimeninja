08ed5fa3b64e65f72792c6f154e6ce5b31cfed64
schneefux <schneefux+commit@schneefux.xyz>
2019-07-17 13:21:14 +0200

    Add page for embeddable meta widget

---
+++ layouts/empty.vue
@@ -0,0 +1,8 @@
+<template>
+    <nuxt />
+</template>
+
+<script>
+export default {
+}
+</script>
---
+++ pages/embed/meta/current.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <h1 class="mt-4 text-2xl text-center">
+      Best Brawlers for Current Events
+    </h1>
+    <h2 class="mt-1 text-sm text-center">
+      Powered by
+      <a
+        class="link-light underline"
+        href="https://brawltime.ninja"
+      >
+        brawltime.ninja
+      </a>
+    </h2>
+    <div class="mt-1 flex flex-wrap justify-center">
+      <div
+        v-for="event in currentEvents"
+        :key="event.id"
+        class="w-80"
+      >
+        <event-card
+          :event="event"
+          class="rounded-lg"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters, mapActions } from 'vuex'
+import { metaStatMaps } from '~/store/index'
+import EventCard from '~/components/event-card'
+
+export default {
+  layout: 'empty',
+  components: {
+    EventCard,
+  },
+  head() {
+    return {
+      title: 'Live Events Meta Widget',
+    }
+  },
+  data() {
+    return {
+      metaStatMaps,
+    }
+  },
+  computed: {
+    ...mapState({
+      currentEvents: state => state.currentEvents,
+    }),
+    ...mapGetters({
+      bestBrawlersByMap: 'bestBrawlersByMap',
+    }),
+  },
+  async fetch({ store }) {
+    if (!process.static) {
+      await store.dispatch('loadCurrentEvents')
+      await store.dispatch('loadMapMeta')
+    }
+  },
+  mounted() {
+    if (process.static) {
+      this.loadCurrentEvents()
+      this.loadMapMeta()
+    }
+  },
+  methods: {
+    ...mapActions({
+      loadMapMeta: 'loadMapMeta',
+      loadCurrentEvents: 'loadCurrentEvents',
+    }),
+  },
+}
+</script>
