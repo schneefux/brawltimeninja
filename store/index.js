146c7b3e7dd12ff31adcb62828d07635b29e648e
schneefux <schneefux+commit@schneefux.xyz>
2019-07-20 17:55:28 +0200

    Rename 'big game' to 'bossfight'

--- store/index.js
+++ store/index.js
@@ -20,7 +20,7 @@ export function induceAdsIntoBrawlers(brawlers, adSlots, adFrequency) {
 export function formatMode(mode) {
   const camelToSnakeCase = str => str.replace(/[A-Z]/g, letter => `_${letter.toLowerCase()}`)
   const capitalize = str => str.replace(/(?:^|\s)\S/g, a => a.toUpperCase())
-  return camelToSnakeCase(mode)
+  return camelToSnakeCase(mode.replace('bigGame', 'bossfight'))
     .split('_')
     .map(w => capitalize(w))
     .join(' ')
