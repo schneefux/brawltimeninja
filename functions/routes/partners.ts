c588514828964f633e3775a3e6d7200ce57d6d27
schneefux <schneefux+commit@schneefux.xyz>
2019-07-28 18:58:54 +0200

    API: Cache BSU articles

--- functions/routes/partners.ts
+++ functions/routes/partners.ts
@@ -1,11 +1,30 @@
 import Router from 'koa-router';
 import Parser from 'rss-parser';
+import cacheManager from 'cache-manager';
+import fsStore from 'cache-manager-fs-hash';
 
 const router = new Router();
 const parser = new Parser();
 
+const cachePath = process.env.CACHE_PATH || 'cache';
+const cacheDisable = !!process.env.CACHE_DISABLE;
+
+const cache = cacheDisable ?
+  cacheManager.caching({
+    store: 'memory',
+    max: 0,
+    ttl: 180,
+  }) :
+  cacheManager.caching(<any>{
+    store: fsStore,
+    options: { path: cachePath, subdirs: true, },
+  });
+
 router.get('/bsu', async (ctx, next) => {
-  const feed = await parser.parseURL('https://brawlstarsup.com/feed/');
+  const feed = <Parser.Output> await cache.wrap(
+    'https://brawlstarsup.com/feed/',
+    () => parser.parseURL('https://brawlstarsup.com/feed/')
+  );
   if (feed.items === undefined) {
     ctx.body = {};
   } else {
