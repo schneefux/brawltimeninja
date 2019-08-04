84134f28ba8614653641212bbc1c5803cf890aad
schneefux <schneefux+commit@schneefux.xyz>
2019-08-04 11:55:26 +0200

    Add embeddable search widget

---
+++ pages/embed/search.vue
@@ -0,0 +1,80 @@
+<template>
+  <form
+    :action="`/player/${cleanedTag}`"
+    class="w-full flex flex-wrap justify-center"
+    target="_blank"
+    @submit="search"
+  >
+    <div class="link-card">
+      <div class="py-2 rounded-lg bg-primary-light">
+        <input
+          v-model="tag"
+          placeholder="Enter your Tag"
+          type="text"
+          autocomplete="off"
+          class="w-40 tracking-wider uppercase placeholder:normal-case font-semibold appearance-none bg-transparent focus:outline-none ml-3 mr-2"
+        >
+        <input
+          type="submit"
+          class="button button-lg mr-3"
+          value="Search"
+        >
+      </div>
+      <p
+        v-if="!tagValid"
+        class="mt-2 -mb-4 text-red-500 w-full text-center"
+      >
+        This is not a tag.
+      </p>
+    </div>
+  </form>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  layout: 'empty',
+  data() {
+    return {
+      tag: undefined,
+      tagValid: false,
+    }
+  },
+  computed: {
+    tagRegex() {
+      return new RegExp(this.tagPattern)
+    },
+    cleanedTag() {
+      return (this.tag || '')
+        .trim()
+        .replace('#', '')
+        .toUpperCase()
+        .replace(/O/g, '0')
+    },
+    ...mapState({
+      tagPattern: state => state.tagPattern,
+    }),
+  },
+  methods: {
+    search(event) {
+      this.tagValid = this.tagRegex.test(this.cleanedTag)
+
+      if (this.tagValid) {
+        this.$ga.event('embed', 'search', 'success')
+      } else {
+        event.preventDefault()
+        this.$ga.event('embed', 'search', 'error_invalid')
+      }
+
+      return this.tagValid
+    },
+  },
+}
+</script>
+
+<style>
+.placeholder\:normal-case::placeholder {
+  @apply normal-case;
+}
+</style>
