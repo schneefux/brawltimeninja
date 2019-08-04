5bd3c8fb5aeaeb824abf98dc16742e7f9a5156f2
schneefux <schneefux+commit@schneefux.xyz>
2019-08-04 19:02:19 +0200

    Refactor cards into CSS classes

--- components/brawler-card.vue
+++ components/brawler-card.vue
@@ -1,15 +1,13 @@
 <template>
-  <div class="card w-80 h-32 bg-primary-dark flex justify-between mx-auto">
-    <div class="w-32 relative">
-      <span class="z-10 absolute left-0 ml-3 mt-2 font-semibold text-white text-2xl text-shadow whitespace-no-wrap capitalize">
-        {{ title.toLowerCase() }}
-      </span>
-      <img
-        :src="require(`~/assets/images/hero/icon/${brawler}_optimized.png`)"
-        class="z-0 absolute bottom-0 h-20"
-      >
-    </div>
-    <div class="py-2 pr-3 flex flex-col justify-center items-end">
+  <div class="card stats-card">
+    <span class="stats-card-title">
+      {{ title.toLowerCase() }}
+    </span>
+    <img
+      :src="require(`~/assets/images/hero/icon/${brawler}_optimized.png`)"
+      class="stats-card-image"
+    >
+    <div class="stats-card-content">
       <div>
         <slot name="history" />
       </div>
