77e005c261776f71b75d2cd701d0f6db0a46f082
schneefux <schneefux+commit@schneefux.xyz>
2019-08-06 19:14:29 +0200

    GA: Fix event tracking affecting bounce rate

--- plugins/analytics.js
+++ plugins/analytics.js
@@ -9,6 +9,8 @@ export default ({ app, env }) => {
       router: app.router,
       set: [
         { field: 'dimension1', value: env.branch },
+        // events should not affect bounce rate
+        { field: 'nonInteraction', value: true },
       ],
       disabled: app.store.state.adsAllowed !== true,
     })
