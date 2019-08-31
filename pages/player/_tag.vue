403ab27797ab9ce8f4713e6a56e6fc1597a0223a
schneefux <schneefux+commit@schneefux.xyz>
2019-08-31 10:08:59 +0200

    Profile: Move install banner below game modes

--- pages/player/_tag.vue
+++ pages/player/_tag.vue
@@ -154,35 +154,6 @@
       </div>
     </div>
 
-    <div
-      v-if="installPrompt !== undefined && !installBannerDismissed"
-      class="mt-10 w-full md:w-1/2 mx-auto text-center leading-tight"
-    >
-      <div class="relative py-3 px-6 bg-primary-darker rounded border-2 border-secondary-lighter">
-        <button
-          class="absolute top-0 right-0 mr-1 mt-1"
-          @click="dismissInstall"
-        >
-          <svg class="h-6 w-6 fill-current" viewBox="0 0 20 20">
-            <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
-          </svg>
-        </button>
-        <p>
-          Install the App for instant access.
-          Track your trophies easily. Light, fast and free.
-        </p>
-        <div class="mt-3">
-          <button
-            class="button button-md"
-            @click="clickInstall"
-          >
-            <span class="mr-1">📥</span>
-            Install
-          </button>
-        </div>
-      </div>
-    </div>
-
     <div class="section-heading">
       <h2 class="text-2xl font-semibold">
         Game Modes
@@ -241,6 +212,35 @@
               and help me unlock Crow?
             </p>
           </div>
+        </div>
+      </div>
+    </div>
+
+    <div
+      v-if="installPrompt !== undefined && !installBannerDismissed"
+      class="mt-10 w-full md:w-1/2 mx-auto text-center leading-tight"
+    >
+      <div class="relative py-3 px-6 bg-primary-darker rounded border-2 border-secondary-lighter">
+        <button
+          class="absolute top-0 right-0 mr-1 mt-1"
+          @click="dismissInstall"
+        >
+          <svg class="h-6 w-6 fill-current" viewBox="0 0 20 20">
+            <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+          </svg>
+        </button>
+        <p>
+          Install the App for instant access.
+          Track your trophies easily. Light, fast and free.
+        </p>
+        <div class="mt-3">
+          <button
+            class="button button-md"
+            @click="clickInstall"
+          >
+            <span class="mr-1">📥</span>
+            Install
+          </button>
         </div>
       </div>
     </div>
