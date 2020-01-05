96a1ea593025c32971e14da377fcfb8c38eb7ff8
schneefux <schneefux+commit@schneefux.xyz>
2020-01-05 17:28:04 +0100

    Work around babel-eslint error by removing eslint

--- nuxt.config.js
+++ nuxt.config.js
@@ -69,16 +69,6 @@ export default {
 
   build: {
     extend(config, ctx) {
-      // Run ESLint on save
-      if (ctx.isDev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-
       if (!ctx.isDev) {
         config.plugins.push(
           new PurgecssPlugin({
