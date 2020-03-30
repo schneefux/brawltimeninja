1d2a8615fe8d83ea352f7f98b0b941088fcc3bd7
schneefux <schneefux+commit@schneefux.xyz>
2020-03-30 09:12:00 +0200

    api: Add missing auth header for rankings

--- api/services/Brawlstars.ts
+++ api/services/Brawlstars.ts
@@ -93,7 +93,13 @@ export default class BrawlstarsService {
   }
 
   public async getTrophiesLeaderboard() {
-    const response = await request<any>('rankings/global/players', apiOfficialUrl, {}, {}, 10000, 600);
+    const response = await request<any>('rankings/global/players',
+      this.apiOfficial,
+      { },
+      { 'Authorization': 'Bearer ' + tokenOfficial },
+      10000,
+      600,
+    );
     return response.items.map((d: any) => ({
       tag: d.tag.replace(/^#/, ''),
       name: d.name,
