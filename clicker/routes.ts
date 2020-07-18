9b2480638da977eee8da0963c222f0654c4a7b86
schneefux <schneefux+commit@schneefux.xyz>
2020-07-18 09:49:54 +0200

    clicker: Set cache headers to 60s

--- clicker/routes.ts
+++ clicker/routes.ts
@@ -20,41 +20,49 @@ router.post('/clicker/track', async (ctx, next) => {
 
 router.get('/clicker/top/exp', async (ctx, next) => {
   ctx.body = await service.getTopByExp(100);
+  ctx.set('Cache-Control', 'public, max-age=60');
   await next();
 });
 
 router.get('/clicker/history/:tag', async (ctx, next) => {
   ctx.body = await service.getHistory(ctx.params.tag);
+  ctx.set('Cache-Control', 'public, max-age=60');
   await next();
 });
 
 router.get('/clicker/winrates/:tag', async (ctx, next) => {
   ctx.body = await service.getPlayerWinrates(ctx.params.tag);
+  ctx.set('Cache-Control', 'public, max-age=60');
   await next();
 });
 
 router.get('/clicker/meta/brawler', async (ctx, next) => {
   ctx.body = await service.getBrawlerMeta(ctx.request.query.trophyrangeLower || '0', ctx.request.query.trophyrangeHigher || '9999');
+  ctx.set('Cache-Control', 'public, max-age=60');
   await next();
 });
 
 router.get('/clicker/meta/starpower', async (ctx, next) => {
   ctx.body = await service.getStarpowerMeta(ctx.request.query.trophyrangeLower || '0', ctx.request.query.trophyrangeHigher || '9999');
+  ctx.set('Cache-Control', 'public, max-age=60');
   await next();
 });
 
 router.get('/clicker/meta/gadget', async (ctx, next) => {
   ctx.body = await service.getGadgetMeta(ctx.request.query.trophyrangeLower || '0', ctx.request.query.trophyrangeHigher || '9999');
+  ctx.set('Cache-Control', 'public, max-age=60');
   await next();
 });
 
 router.get('/clicker/meta/mode', async (ctx, next) => {
   ctx.body = await service.getModeMeta(ctx.request.query.trophyrangeLower || '0', ctx.request.query.trophyrangeHigher || '9999');
+  ctx.set('Cache-Control', 'public, max-age=60');
   await next();
 });
 
 router.get('/clicker/meta/map', async (ctx, next) => {
   ctx.body = await service.getMapMeta(ctx.request.query.trophyrangeLower || '0', ctx.request.query.trophyrangeHigher || '9999');
+  ctx.set('Cache-Control', 'public, max-age=60');
   await next();
 });
 
