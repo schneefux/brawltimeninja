9b5b3b3604502fbe6700b272506eea5bf1cf1484
schneefux <schneefux+commit@schneefux.xyz>
2020-08-28 21:52:06 +0200

    media: Enable CORS

--- media/server.ts
+++ media/server.ts
@@ -1,10 +1,12 @@
 import Koa from 'koa';
+import cors from '@koa/cors';
 import bodyParser from 'koa-bodyparser';
 
 import mediaRoutes from './routes';
 
 const app = new Koa();
 
+app.use(cors({ origin: '*' })); // TODO for development only
 app.use(bodyParser());
 app.use(mediaRoutes);
 
