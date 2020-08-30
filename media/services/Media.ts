9b0195fa21d57815665c59c0bae042699e8f1ebd
schneefux <schneefux+commit@schneefux.xyz>
2020-08-30 09:34:41 +0200

    media: Fix 8-Bit and Rico info

--- media/services/Media.ts
+++ media/services/Media.ts
@@ -110,7 +110,7 @@ export default class MediaService {
     }
 
     const id = brawlerId({ name: name })
-    const character = characters.find(c => (c.itemName || '').replace(/\./g, '') == id.replace(/_/g, ''))
+    const character = characters.find(c => (c.itemName || '').replace(/\.-/g, '').replace('ricochet', 'rico') == id.replace(/_/g, ''))
     if (character == undefined) {
       console.log('brawler not found: ' + name)
       return null
