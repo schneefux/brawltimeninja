41b8608040fc922045fa3959ab566a0eac1904dc
schneefux <schneefux+commit@schneefux.xyz>
2020-09-05 11:50:07 +0200

    web: Fix brawler card title border too wide

--- web/components/brawler-card.tsx
+++ web/components/brawler-card.tsx
@@ -17,28 +17,29 @@ export default Vue.extend({
     },
   },
   render(h, { props, scopedSlots }) {
-    return <div class="card stats-card">
+    return <div class="card w-80 max-w-full bg-primary-dark mx-auto relative">
       <div class="relative">
         <div class="z-10 absolute left-0 ml-1 mt-1 w-5/12">
-          <div class="bg-black bg-opacity-75 rounded-lg px-2">
-            <span class="font-semibold text-white text-2xl text-shadow capitalize ">
+          <div class="bg-black bg-opacity-75 rounded-lg px-2 w-fit-content">
+            <span class="font-semibold text-white text-2xl text-shadow capitalize">
               { props.title.toLowerCase() }
             </span>
           </div>
-        </div>        <media-img
+        </div>
+        <media-img
           path={'/brawlers/' + props.brawler + '/avatar'}
           size="160"
-          clazz="stats-card-image"
+          clazz="z-0 absolute bottom-0 h-20"
         ></media-img>
         { props.icon !== '' ?
           <media-img
             path={props.icon}
             size="80"
-            clazz="stats-card-icon"
+            clazz="absolute bottom-0 h-10 rounded-full p-1 ml-20 bg-black bg-opacity-50"
           ></media-img>
           : ''
         }
-        <div class="stats-card__content">
+        <div class="py-2 pr-3 flex flex-col justify-center items-end">
           <div>
             { scopedSlots.stats({}) }
           </div>
