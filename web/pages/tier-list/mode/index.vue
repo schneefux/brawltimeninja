76a1fc7359a62f96bfdde938a3ada2bd16beb2e9
schneefux <schneefux+commit@schneefux.xyz>
2020-09-06 11:21:04 +0200

    web: Improve modes page performance

--- web/pages/tier-list/mode/index.vue
+++ web/pages/tier-list/mode/index.vue
@@ -14,13 +14,15 @@
       </p>
     </div>
 
-    <adsense
-      ins-class="ad-section"
-      data-ad-client="ca-pub-6856963757796636"
-      data-ad-slot="5576993339"
-      data-ad-format="auto"
-      data-full-width-responsive
-    />
+    <client-only>
+      <adsense
+        ins-class="ad-section"
+        data-ad-client="ca-pub-6856963757796636"
+        data-ad-slot="5576993339"
+        data-ad-format="auto"
+        data-full-width-responsive
+      />
+    </client-only>
 
     <div
       class="section"
@@ -30,38 +32,35 @@
       }"
     >
       <div class="flex flex-wrap justify-center">
-        <nuxt-link
+        <mode-best-brawlers-card
           v-for="mode in modes"
           :key="mode"
-          :to="`/tier-list/mode/${camelToKebab(mode)}`"
-        >
-          <mode-best-brawlers-card
-            :mode="mode"
-            :top-brawlers="topBrawlersByMode[mode]"
-          ></mode-best-brawlers-card>
-        </nuxt-link>
+          :mode="mode"
+          :top-brawlers="topBrawlersByMode[mode]"
+        ></mode-best-brawlers-card>
       </div>
     </div>
 
-    <adsense
-      v-if="!isApp"
-      ins-class="ad-section"
-      data-ad-client="ca-pub-6856963757796636"
-      data-ad-slot="6913006175"
-      data-ad-format="auto"
-      data-full-width-responsive
-    />
+    <client-only>
+      <adsense
+        v-if="!isApp"
+        ins-class="ad-section"
+        data-ad-client="ca-pub-6856963757796636"
+        data-ad-slot="6913006175"
+        data-ad-format="auto"
+        data-full-width-responsive
+      />
+    </client-only>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import { mapState, mapActions } from 'vuex'
-import { getMostPopular, metaStatMaps, formatMode, camelToKebab } from '../../../lib/util'
+import { mapState } from 'vuex'
+import { getMostPopular, MetaGridEntrySorted } from '../../../lib/util'
 import { ModeMetaMap } from '../../../model/MetaEntry'
 
 export default Vue.extend({
-  name: 'ModeMetaPage',
   head() {
     const description = 'Brawl Stars Game Mode Tier Lists. Find the best Brawlers. View Win Rates and Rankings.'
     return {
@@ -74,29 +73,24 @@ export default Vue.extend({
   },
   data() {
     return {
-      modeMeta: {} as ModeMetaMap,
-      camelToKebab,
-      formatMode,
-      metaStatMaps,
+      modes: [] as string[],
+      topBrawlersByMode: {} as { [key: string]: MetaGridEntrySorted[] },
     }
   },
   computed: {
-    modes() {
-      const modeMeta = this.modeMeta as ModeMetaMap
-      return [...Object.keys(modeMeta)]
-        .sort((mode1, mode2) => modeMeta[mode2].sampleSize - modeMeta[mode1].sampleSize)
-    },
-    topBrawlersByMode() {
-      return getMostPopular((<any>this).modeMeta as ModeMetaMap)
-    },
     ...mapState({
       isApp: (state: any) => state.isApp as boolean,
     }),
   },
   async asyncData({ $axios }) {
     const modeMeta = await $axios.$get<ModeMetaMap>('/api/meta/mode')
+    const modes = [...Object.keys(modeMeta)]
+      .sort((mode1, mode2) => modeMeta[mode2].sampleSize - modeMeta[mode1].sampleSize)
+    const topBrawlersByMode = getMostPopular(modeMeta)
+
     return {
-      modeMeta,
+      modes,
+      topBrawlersByMode,
     }
   },
   methods: {
