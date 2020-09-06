cbb65dd71c23bd3a8e039fe32ed905ab83085c16
schneefux <schneefux+commit@schneefux.xyz>
2020-09-06 10:29:43 +0200

    web: Lazy-load tag help images

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -71,11 +71,13 @@
         <div class="mt-2 card card--dark card__content">
           <p class="font-semibold">Tap on your profile icon.</p>
           <img
+            loading="lazy"
             src="~/assets/images/tag/tag-1.jpg"
             class="mt-1 w-80 max-w-full"
           >
           <p class="mt-3 font-semibold">The string starting with "#" is your tag.</p>
           <img
+            loading="lazy"
             src="~/assets/images/tag/tag-2.jpg"
             class="mt-1 w-80 max-w-full"
           >
