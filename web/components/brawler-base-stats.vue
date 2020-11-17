4404c06bfbb3c3da489ffc19d7bb3cc1a2921954
schneefux <schneefux+commit@schneefux.xyz>
2020-11-17 21:42:12 +0100

    web: Add texts to brawler pages

--- web/components/brawler-base-stats.vue
+++ web/components/brawler-base-stats.vue
@@ -15,9 +15,7 @@
             class="w-full"
           >
             <dt class="card__header">{{ brawlerName }}</dt>
-            <dd class="card__text mb-3 whitespace-pre-line">
-              {{ info.description }}
-            </dd>
+            <dd class="card__text mb-3 whitespace-pre-line">{{ brawlerDescription }}</dd>
             <div class="flex justify-between">
               <dt class="font-semibold">Health at Level 1</dt>
               <dd>{{ info.health }}</dd>
@@ -48,6 +46,7 @@
         </dt>
         <dd class="card__text mb-3 h-full whitespace-pre-line">
           {{ info[attack].description }}
+          {{ content[attack] }}
         </dd>
         <div
           v-if="info[attack].rechargeTime != null"
@@ -105,8 +104,9 @@
       v-if="data != null"
       class="card card--dark card--sm card__content"
     >
-      <span class="card__header">{{ brawlerName }} Statistics</span>
-      <dl class="card__text">
+      <dl>
+        <dt class="card__header">{{ brawlerName }} Statistics</dt>
+        <dd class="card__text mb-3 whitespace-pre-line">{{ statisticsDescription }}</dd>
         <div class="flex justify-between">
           <dt class="font-semibold">Use Rate</dt>
           <dd>{{ metaStatMaps.formatters.useRate(data.picks_weighted / totals.picks_weighted) }}</dd>
@@ -126,7 +126,8 @@
 
 <script lang="ts">
 import Vue, { PropType } from 'vue'
-import { metaStatMaps } from '~/lib/util'
+import { mapState } from 'vuex'
+import { capitalize, metaStatMaps } from '~/lib/util'
 import { BrawlerData } from '~/model/Media'
 
 interface Row {
@@ -134,6 +135,16 @@ interface Row {
   picks_weighted: number
   battle_victory: number
   battle_starplayer: number
+}
+
+interface Content {
+  brawler?: string
+  main?: string
+  super?: string
+
+  gender: string
+  pronoun: string
+  possessivePronoun: string
 }
 
 export default Vue.extend({
@@ -152,10 +163,47 @@ export default Vue.extend({
       info: null as BrawlerData|null,
       data: null as Row|null,
       totals: null as Row|null,
+      content: {
+        gender: 't',
+        pronoun: 'they',
+        possessivePronoun: 'their',
+      } as Content,
     }
   },
+  fetchDelay: 0,
   async fetch() {
     this.info = await this.$axios.$get<BrawlerData|null>(`${process.env.mediaUrl}/brawlers/${this.brawlerId}/info`).catch(() => null)
+
+    const detectGender = (str: string) => {
+      const text = str.toLowerCase()
+      if (text.includes(' she ') || text.includes(' her ')) {
+        return 'f'
+      }
+      if (text.includes(' he ') || text.includes(' his ') || text.includes(' him ')) {
+        return 'm'
+      }
+      return 't'
+    }
+
+    const pronouns = {
+      'm': 'he',
+      'f': 'she',
+      't': 'they',
+    }
+    const possessivePronouns = {
+      'm': 'his',
+      'f': 'her',
+      't': 'their',
+    }
+
+    const gender = detectGender(this.info?.description || '')
+    const content = await this.$content(`/brawlers/${this.brawlerId}`).fetch().catch(err => ({}))
+    this.content = {
+      gender,
+      pronoun: pronouns[gender],
+      possessivePronoun: possessivePronouns[gender],
+      ...content,
+    }
 
     const data = await this.$clicker.query<Row>('meta.brawler.base-stats-widget', 'map',
       ['brawler_name'],
@@ -169,9 +217,54 @@ export default Vue.extend({
     this.totals = data.totals
   },
   computed: {
+    brawlerDescription(): string {
+      if (this.info == null) {
+        return ''
+      }
+      return `
+        ${this.info.description}
+        ${this.info.class ? `${this.brawlerName} is a ${this.info.class}${this.info.rarity && ` with ${this.info.rarity} Rarity.`}` : (this.info.rarity && `${this.brawlerName}'s Rarity is ${this.info.rarity}.`)}\
+        ${capitalize(this.content.pronoun)} is unlocked ${this.info.unlock ? this.info.unlock == 0 ? `when starting the game` : `upon reaching ${this.info.unlock} Trophies` : `by opening Brawl Boxes`}.
+        ${this.content?.brawler || ''}\
+      `
+    },
+    statisticsDescription(): string {
+      if (this.data == undefined || this.totals == undefined) {
+        return ''
+      }
+
+      const clamp = (min: number, max: number, n: number) => Math.min(max, Math.max(min, n))
+      const minMaxScale = (fromMin: number, fromMax: number, n: number) => (n - fromMin) / (fromMax - fromMin)
+      const scaleInto = (fromMin: number, fromMax: number, toMax: number, n: number) => clamp(0, toMax, Math.floor(minMaxScale(fromMin, fromMax, n) * toMax))
+
+      const popularityWords = ['niche', 'not so popular', 'moderately popular', 'very popular']
+      const useRate = this.data.picks_weighted / this.totals.picks_weighted
+      const popularity = popularityWords[scaleInto(0.02, 0.03, popularityWords.length - 1, useRate)]
+
+      const metanessWords = ['not that good', 'below average', 'average', 'above average', 'excellent']
+      const metaness = metanessWords[scaleInto(0.55, 0.60, metanessWords.length - 1, this.data.battle_victory)]
+
+      const overallWords = [
+        `Unless you know how to play ${this.brawlerName} well, you should pick a different Brawler.`,
+        `${this.brawlerName} is not the best choice at the moment.`,
+        `Overall, ${this.brawlerName} is a solid pick.`,
+        `Playing ${this.brawlerName} is a good Brawler at the moment.`,
+        `${this.brawlerName} belongs to the best Brawlers overall.`,
+      ]
+      const overall = overallWords[scaleInto(0.02*0.55, 0.03*0.60, overallWords.length - 1, useRate * this.data.battle_victory)]
+
+      return `
+        ${this.brawlerName} is, judging by ${this.content.possessivePronoun} Use Rate, a ${popularity} Brawler.\
+        Looking at ${this.content.possessivePronoun} Win Rate, ${this.content.pronoun} is ${metaness} in the current Meta.\
+        ${overall}
+      `
+    },
     metaStatMaps() {
       return metaStatMaps
     },
+    ...mapState({
+      totalBrawlers: (state: any) => state.totalBrawlers,
+    })
   },
 })
 </script>
