7bbbebc972971b49b27870aabe60cb661e86f60c
schneefux <schneefux+commit@schneefux.xyz>
2021-11-24 10:17:50 +0100

    web: Fix Google optimize integration

--- web/plugins/gtag.ts
+++ web/plugins/gtag.ts
@@ -3,23 +3,25 @@ import Vue from 'vue'
 import VueGtag from 'vue-gtag'
 
 const plugin: Plugin = ({ app }) => {
+  const params = {
+    optimize_id: 'OPT-PWZ78LC',
+    custom_map: {
+      'dimension1': 'branch',
+      'dimension2': 'ads_blocked',
+      'dimension3': 'is_pwa',
+      'dimension4': 'is_twa',
+      'dimension5': 'test_group',
+    },
+  }
   Vue.use(VueGtag, {
     config: {
       id: 'G-8GGHZC6QR2',
-      params: {
-        optimize_id: 'OPT-PWZ78LC',
-        custom_map: {
-          'dimension1': 'branch',
-          'dimension2': 'ads_blocked',
-          'dimension3': 'is_pwa',
-          'dimension4': 'is_twa',
-          'dimension5': 'test_group',
-        },
-      },
+      params,
     },
     includes: [ {
       // old property
       id: 'UA-137233906-1',
+      params,
     } ],
     // toggled in layout.vue
     enabled: app.store!.state.adsAllowed == true,
