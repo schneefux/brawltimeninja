035800f29ffffbd71c8c9fa686dd5c5a00c1c315
schneefux <schneefux+commit@schneefux.xyz>
2022-01-16 14:57:28 +0100

    manager: Store sessions in database

--- manager/authentication.ts
+++ manager/authentication.ts
@@ -2,6 +2,9 @@ import { Params } from '@feathersjs/feathers'
 import { expressOauth, OAuthProfile, OAuthStrategy } from "@feathersjs/authentication-oauth"
 import { Application } from "@feathersjs/express/lib"
 import { AuthenticationService, JWTStrategy } from '@feathersjs/authentication'
+import session from 'express-session'
+import knexSession from 'connect-session-knex'
+import knex from 'knex'
 
 class GoogleStrategy extends OAuthStrategy {
   async getEntityData(profile: OAuthProfile, existing: any, params: Params) {
@@ -16,5 +19,14 @@ export default (app: Application) => {
   authentication.register('jwt', new JWTStrategy());
   authentication.register('google', new GoogleStrategy());
   app.use('/authentication', authentication)
-  app.configure(expressOauth())
+  app.configure(expressOauth({
+    expressSession: session({
+      secret: app.get('authentication.secret'),
+      resave: false,
+      saveUninitialized: false,
+      store: new (knexSession(session))({
+        knex: knex(app.get('database')),
+      })
+    })
+  }))
 }
