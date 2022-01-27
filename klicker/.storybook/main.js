e66eb34b11469181e0fae3e6a591535451b33836
schneefux <schneefux+commit@schneefux.xyz>
2022-01-27 09:31:46 +0100

    klicker: Add story source addon

--- klicker/.storybook/main.js
+++ klicker/.storybook/main.js
@@ -10,6 +10,7 @@ module.exports = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
+    '@storybook/addon-storysource',
     'storybook-dark-mode',
     {
       name: '@storybook/addon-postcss',
