cc16971118180f98d4a9fba09f1d27ea3e1379c3
schneefux <schneefux+commit@schneefux.xyz>
2022-02-13 19:21:26 +0100

    web: Update ios install guide layout

--- web/pages/install/ios.vue
+++ web/pages/install/ios.vue
@@ -1,47 +1,43 @@
 <template>
-  <page title="Adding Brawl Time Ninja to your Home Screen">
-    <ol class="flex flex-wrap justify-center">
-      <li>
-        <b-card
-          title="1. Tap the &quot;Share&quot; Icon"
-          xs
-        >
-          <img
-            slot="content"
-            src="~/assets/images/install/ios-1_arrow.jpg"
-          >
-        </b-card>
-      </li>
-      <li>
-        <b-card
-          title="2. &quot;Add to Home Screen&quot;"
-          xs
-        >
-          <img
-            slot="content"
-            src="~/assets/images/install/ios-2_arrow.jpg"
-          >
-        </b-card>
-      </li>
-      <li>
-        <b-card
-          title="3. &quot;Add&quot;"
-          xs
-        >
-          <img
-            slot="content"
-            src="~/assets/images/install/ios-3_arrow.jpg"
-          >
-        </b-card>
-      </li>
-    </ol>
+  <page :title="$t('install.title')">
+    <page-section>
+      <ol class="flex flex-wrap justify-center gap-8">
+        <li>
+          <b-card :title="$t('install.ios.tap-share')">
+            <img
+              slot="content"
+              src="~/assets/images/install/ios-1_arrow.jpg"
+              class="max-w-xs"
+            >
+          </b-card>
+        </li>
+        <li>
+          <b-card :title="$t('install.ios.add-to-home')">
+            <img
+              slot="content"
+              src="~/assets/images/install/ios-2_arrow.jpg"
+              class="max-w-xs"
+            >
+          </b-card>
+        </li>
+        <li>
+          <b-card :title="$t('install.ios.confirm')">
+            <img
+              slot="content"
+              src="~/assets/images/install/ios-3_arrow.jpg"
+              class="max-w-xs"
+            >
+          </b-card>
+        </li>
+      </ol>
+    </page-section>
   </page>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import { defineComponent } from '@nuxtjs/composition-api'
 
-export default Vue.extend({
+export default defineComponent({
   middleware: ['cached'],
   meta: {
     title: 'Install',
