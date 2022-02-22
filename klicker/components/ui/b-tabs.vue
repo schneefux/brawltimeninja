58d18037cc6f10803a80bebab1e1d79eb55facfd
schneefux <schneefux+commit@schneefux.xyz>
2022-02-22 19:32:51 +0100

    klicker: Fix vertical scrolling when one tab is very tall

--- klicker/components/ui/b-tabs.vue
+++ klicker/components/ui/b-tabs.vue
@@ -2,7 +2,7 @@
   <div ref="container">
     <nav
       :class="navClass"
-      class="sticky md:static bg-gray-100 dark:bg-gray-900 border-b-2 border-black/[0.10] dark:border-white/[0.10]"
+      class="sticky bg-gray-100 dark:bg-gray-900 border-b-2 border-black/[0.10] dark:border-white/[0.10]"
     >
       <ul class="flex mx-6 gap-x-8 overflow-x-auto">
         <li
@@ -12,8 +12,8 @@
           <button
             class="text-lg px-8 py-2 whitespace-nowrap transition duration-100 ease-in-out"
             :class="{
-              'border-primary-400 border-b-2': tab.id == activeTab,
-              'hover:border-primary-200 hover:border-b-2 text-gray-800/75 dark:text-gray-200/75 hover:text-gray-200 dark:hover:text-gray-200': tab.id != activeTab,
+              'border-primary-400 border-b-2': tab.id == activeTab || tabVisibility[tab.id],
+              'hover:border-primary-200 hover:border-b-2 text-gray-800/75 dark:text-gray-200/75 hover:text-gray-200 dark:hover:text-gray-200': !(tab.id == activeTab || tabVisibility[tab.id]),
             }"
             @click="setActiveTab(tab)"
           >
@@ -25,13 +25,18 @@
 
     <div
       ref="tabContainer"
-      class="mt-4 snap-x snap-mandatory flex overflow-x-auto hide-scrollbar"
+      class="mt-4 snap-x snap-mandatory grid auto-cols-[100%] grid-flow-col gap-x-8 overflow-x-auto hide-scrollbar"
     >
       <div
         v-for="tab in tabs"
         :key="tab.id"
         :ref="tab.id"
-        class="mr-8 w-full flex-shrink-0 snap-center snap-always"
+        :class="{
+          // shrink pages that are outside of the viewport
+          // so that the active page does not grow
+          'h-screen overflow-y-auto hide-scrollbar': tab.id != activeTab,
+        }"
+        class="snap-center snap-always"
       >
         <slot :name="tab.id"></slot>
       </div>
@@ -61,30 +66,45 @@ export default defineComponent({
   },
   // TODO replace refs by function ref when migrating to Vue 3
   setup(props, { refs }) {
-    const container = ref<HTMLElement|null>(null)
-    const tabContainer = ref<HTMLElement|null>(null)
-    const activeTab = ref(props.tabs[0].id)
+    const container = ref<HTMLElement|null>()
+    const tabContainer = ref<HTMLElement|null>()
+    const activeTab = ref<string>(props.tabs[0].id)
+    const tabVisibility = ref<Record<string, boolean>>({
+      [activeTab.value]: true,
+    })
 
     const setActiveTab = (tab: Tab) => {
       const tabElement = refs[tab.id][0] as HTMLElement
       const left = tabContainer.value!.scrollLeft + tabElement.getBoundingClientRect().left - tabContainer.value!.getBoundingClientRect().left
       tabContainer.value!.scrollTo({ left, behavior: 'smooth' })
-
-      if (container.value!.getBoundingClientRect().top < 0) {
-        // TODO does not work
-        container.value!.scrollIntoView({ behavior: 'smooth' })
-      }
     }
 
     onMounted(() => {
       for (const tab of props.tabs) {
-        if (tab.id in refs && (refs[tab.id] as HTMLElement[]).length > 0) {
-          useIntersectionObserver(refs[tab.id][0], ([{ isIntersecting }]) => {
-            if (isIntersecting) {
-              activeTab.value = tab.id
+        const tabElement = refs[tab.id][0] as HTMLElement
+
+        useIntersectionObserver(tabElement, ([{ isIntersecting }]) => {
+          tabVisibility.value = {
+            ...tabVisibility.value,
+            [tab.id]: isIntersecting,
+          }
+        }, {
+          root: container.value,
+          threshold: 0.25,
+        })
+
+        useIntersectionObserver(tabElement, ([{ isIntersecting }]) => {
+          if (isIntersecting) {
+            activeTab.value = tab.id
+
+            if (container.value!.getBoundingClientRect().top < 0) {
+              container.value!.scrollIntoView()
             }
-          })
-        }
+          }
+        }, {
+          root: container.value,
+          threshold: 1.0,
+        })
       }
     })
 
@@ -93,6 +113,7 @@ export default defineComponent({
       tabContainer,
       setActiveTab,
       activeTab,
+      tabVisibility,
     }
   },
 })
