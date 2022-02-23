adfe26d62e6739243d3dc5d475013bb5ad4f280f
schneefux <schneefux+commit@schneefux.xyz>
2022-02-23 19:52:31 +0100

    web: Refactor layout/default.vue

--- web/layouts/default.vue
+++ web/layouts/default.vue
@@ -38,84 +38,86 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import { MetaInfo } from 'vue-meta'
-import { mapState, mapMutations } from 'vuex'
+import { computed, defineComponent, useContext, useMeta, useStore, watch, wrapProperty } from '@nuxtjs/composition-api'
 
-export default Vue.extend({
-  head(): MetaInfo {
+const useGtag = wrapProperty('$gtag', false)
+export default defineComponent({
+  head: {},
+  setup(props, { root }) {
+    const { localePath } = useContext()
+
+    useMeta(() => {
     // https://i18n.nuxtjs.org/seo/#improving-performance
     // will also add rel=canonical without query params
-    return this.$nuxtI18nHead({ addSeoAttributes: true, addDirAttribute: true })
-  },
-  computed: {
-    links(): { name: string, target: string }[] {
-      return [ {
+      return root.$nuxtI18nHead({ addSeoAttributes: true, addDirAttribute: true })
+    })
+
+    const links = [ {
         name: 'Leaderboards',
-        target: this.localePath('/leaderboard/hours'),
+      target: localePath('/leaderboard/hours'),
       }, {
         name: 'Guides',
         target: '/blog/guides',
       }, {
         name: 'Status',
-        target: this.localePath('/status'),
+      target: localePath('/status'),
       }, {
         name: 'Privacy',
         target: '/about',
       }]
-    },
-    ...mapState({
-      version: (state: any) => state.version as number,
-      adsAllowed: (state: any) => state.adsAllowed as boolean,
-      cookiesAllowed: (state: any) => state.cookiesAllowed as boolean,
-      consentPopupVisible: (state: any) => state.consentPopupVisible as boolean,
-      isApp: (state: any) => state.isApp as boolean,
-    }),
-  },
-  methods: {
-    disableCookies() {
-      this.hideConsentPopup()
-      this.disallowCookies()
-      this.disallowAds()
-      this.clearAdsCookie()
-      this.clearCookieCookie()
-      this.hideAds()
-    },
-    enableCookies() {
-      this.hideConsentPopup()
-      this.allowCookies()
-      this.disallowAds()
-      this.clearAdsCookie()
-      this.setCookieCookie()
-      this.hideAds()
-    },
-    enableCookiesAndAds() {
-      this.hideConsentPopup()
-      this.allowCookies()
-      this.allowAds()
-      this.setAdsCookie()
-      this.setCookieCookie()
-      this.enableAds()
-    },
-    setCookieCookie() {
+
+    const store = useStore<any>()
+    const version = computed(() => store.state.version as number)
+    const adsAllowed = computed(() => store.state.adsAllowed as boolean)
+    const cookiesAllowed = computed(() => store.state.cookiesAllowed as boolean)
+    const consentPopupVisible = computed(() => store.state.consentPopupVisible as boolean)
+
+    const disableCookies = () => {
+      store.commit('hideConsentPopup')
+      store.commit('disallowCookies')
+      store.commit('disallowAds')
+      clearAdsCookie()
+      clearCookieCookie()
+      hideAds()
+    }
+    const enableCookies = () => {
+      store.commit('hideConsentPopup')
+      store.commit('allowCookies')
+      store.commit('disallowAds')
+      clearAdsCookie()
+      setCookieCookie()
+      hideAds()
+    }
+    const enableCookiesAndAds = () => {
+      store.commit('hideConsentPopup')
+      store.commit('allowCookies')
+      store.commit('allowAds')
+      setAdsCookie()
+      setCookieCookie()
+      enableAds()
+    }
+
+    const setCookieCookie = () => {
       document.cookie = `cookies=true; expires=${new Date(Date.now() + 365*24*60*60*1000)}`
-    },
-    clearCookieCookie() {
+    }
+    const clearCookieCookie = () => {
       document.cookie = `cookies=; expires=${new Date(0)}`
-    },
-    setAdsCookie() {
+    }
+    const setAdsCookie = () => {
       document.cookie = `ads=true; expires=${new Date(Date.now() + 365*24*60*60*1000)}`
-    },
-    clearAdsCookie() {
+    }
+    const clearAdsCookie = () => {
       document.cookie = `ads=; expires=${new Date(0)}`
-    },
-    enableAds() {
-      if (this.adsAllowed && (<any>process).client) {
+    }
+
+    const gtag = useGtag()
+    const enableAds = () => {
+      if (adsAllowed.value && (<any>process).client) {
         // update consent preferences
         if ('adsbygoogle' in window) {
           (<any>window).adsbygoogle.pauseAdRequests = 0
         }
-        this.$gtag.optIn()
+        gtag.optIn()
 
         // track some meta data
         // play store allows only 1 ad/page - TWA is detected via referrer
@@ -123,24 +125,24 @@ export default Vue.extend({
         const isTwa = document.referrer.startsWith('android-app')
 
         if (isPwa || isTwa) {
-          this.setIsApp()
+          store.commit('setIsApp')
         }
 
-        this.$gtag.event('branch_dimension', {
+        gtag.event('branch_dimension', {
           'branch': process.env.branch || '',
           'non_interaction': true,
         })
-        this.$gtag.event('is_pwa_dimension', {
+        gtag.event('is_pwa_dimension', {
           'is_pwa': isPwa,
           'non_interaction': true,
         })
-        this.$gtag.event('is_twa_dimension', {
+        gtag.event('is_twa_dimension', {
           'is_twa': isTwa,
           'non_interaction': true,
         })
       }
-    },
-    hideAds() {
+    }
+    const hideAds = () => {
       if ((<any>process).client) {
         (<any>window).adsbygoogle.pauseAdRequests = 1
         const sheet = document.createElement('style')
@@ -148,36 +150,34 @@ export default Vue.extend({
         sheet.innerText = '.adswrapper { display: none; }'
         document.head.appendChild(sheet)
       }
-    },
-    ...mapMutations({
-      allowCookies: 'allowCookies',
-      disallowCookies: 'disallowCookies',
-      allowAds: 'allowAds',
-      disallowAds: 'disallowAds',
-      hideConsentPopup: 'hideConsentPopup',
-      showConsentPopup: 'showConsentPopup',
-      setIsApp: 'setIsApp',
-    }),
-  },
-  watch: {
+    }
+
     // called after vuex-persist has loaded
-    version() {
-      if (!this.cookiesAllowed) {
-        this.showConsentPopup()
+    watch(version, () => {
+      if (!cookiesAllowed.value) {
+        store.commit('showConsentPopup')
       } else {
         // 'unpack-store' middleware sets cookiesAllowed and adsAllowed
         // based on cookies
         // 2020-12-06: Disabled because caching the HTML with the `cached`
         // middleware will serve random settings to users
-        this.setCookieCookie() // refresh
-        if (this.adsAllowed) {
-          this.enableAds()
-          this.setAdsCookie() // refresh
+        setCookieCookie() // refresh
+        if (adsAllowed.value) {
+          enableAds()
+          setAdsCookie() // refresh
         } else {
-          this.hideAds()
+          hideAds()
         }
       }
-    },
+    })
+
+    return {
+      links,
+      disableCookies,
+      enableCookies,
+      enableCookiesAndAds,
+      consentPopupVisible,
+    }
   },
 })
 </script>
