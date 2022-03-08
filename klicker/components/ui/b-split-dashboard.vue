d0e61ac151121000129f3c0477694e7fd20c0f26
schneefux <schneefux+commit@schneefux.xyz>
2022-03-08 19:55:13 +0100

    klicker: Adjust split-dashboard gap

--- klicker/components/ui/b-split-dashboard.vue
+++ klicker/components/ui/b-split-dashboard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grid grid-cols-1 lg:grid-cols-[16rem,minmax(0,1fr)] gap-x-8">
+  <div class="grid grid-cols-1 lg:grid-cols-[16rem,minmax(0,1fr)] gap-8">
     <div>
       <div class="lg:sticky lg:top-0 max-w-md">
         <slot name="aside"></slot>
