19c4bea637ddebabdd9b2f6ef3c4c42f14061d20
schneefux <schneefux+commit@schneefux.xyz>
2022-04-06 10:00:03 +0200

    scripts: Remove old apk converter

--- scripts/update_assets.sh
+++ scripts/update_assets.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-node download_apk.js
-unzip -q -o brawlstars.apk -d brawlstars
-python csv_to_json.py
-node prepare_json.js
 node download_maps.js
 node download_translations.js
+node scraper.js
 sftp -r -P 2222 -i ~/.ssh/brawlbot.key brawlbot@ssh.brawltime.ninja << EOT
   put ./static/* /brawlbot/assets/
   put ./out/* /brawlbot/assets/
