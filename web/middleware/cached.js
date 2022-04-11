e3ebeec5168b3a17b03c54c5084e676e3d35ddbe
schneefux <schneefux+commit@schneefux.xyz>
2022-04-11 16:55:24 +0200

    web: Cache page by cookie value

--- web/middleware/cached.js
+++ web/middleware/cached.js
@@ -1,5 +1,6 @@
 export default function ({ res }) {
-  if(res != undefined) {
+  if (res != undefined) {
+    res.setHeader('Vary', 'Cookie') // cache every variation of cookies
     res.setHeader('Cache-Control', 'public, max-age=60')
   }
 }
