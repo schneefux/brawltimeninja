dcb5954543d15a400d52fb4a53f696c277f5bcb1
Yura Bobalo <Yura.bob331@gmail.com>
2022-05-09 11:34:47 +0300

    Added Ukrainian language
    shouted out my website: yura331.com
    Hope you dont mind 👉👈

--- web/lang/index.js
+++ web/lang/index.js
@@ -1,9 +1,10 @@
 import en from './en.json'
 import de from './de.json'
 import es from './es.json'
+import ua from './ua.json'
 
 // TODO lazy-load these from media or static later
-const staticStrings = { en, de, es }
+const staticStrings = { en, de, es, ua }
 
 async function getTraduoraStrings($http, $config, locale) {
   const tokenResponse = await $http.$post(`${$config.traduoraUrl}/api/v1/auth/token`, {
