2f57fa9fc3d841f5800e9d1ae4cc25e70a45d0cb
schneefux <schneefux+commit@schneefux.xyz>
2022-05-28 19:01:40 +0200

    web: Import fontawesome CSS, fixes layout shift

--- web/plugins/fontawesome.ts
+++ web/plugins/fontawesome.ts
@@ -1,4 +1,5 @@
 import Vue from 'vue'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import '@fortawesome/fontawesome-svg-core/styles.css'
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
