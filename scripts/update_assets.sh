2628caed76fa42cdd4dbf587735ff52e03a0b21d
schneefux <schneefux+commit@schneefux.xyz>
2022-08-15 11:34:43 +0200

    scripts: Quiet sftp

--- scripts/update_assets.sh
+++ scripts/update_assets.sh
@@ -3,7 +3,7 @@
 node download_maps.js
 node download_translations.js
 node scraper.js
-sftp -r -P 2222 -i ~/.ssh/brawlbot.key brawlbot@ssh.brawltime.ninja << EOT
+sftp -q -r -P 2222 -i ~/.ssh/brawlbot.key brawlbot@ssh.brawltime.ninja << EOT
   put ./static/* /brawlbot/assets/
   put ./out/* /brawlbot/assets/
   quit
