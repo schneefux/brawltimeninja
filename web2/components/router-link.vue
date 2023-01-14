ae72b9400cfbeb848ccc182656d465eb1d991666
schneefux <schneefux+commit@schneefux.xyz>
2023-01-14 17:48:48 +0100

    web: Implement "active" router-link

--- web2/components/router-link.vue
+++ web2/components/router-link.vue
@@ -1,12 +1,19 @@
 <template>
-  <a :href="href">
+  <a
+    :href="href"
+    :class="{
+      [exactActiveClass]: exactActive,
+      [activeClass]: active,
+    }"
+  >
     <slot></slot>
   </a>
 </template>
 
 <script lang="ts">
 import { computed, defineComponent, PropType } from 'vue'
-import { useRouter, Route } from '@/composables/compat'
+import { useRouter, Route, useRoute } from '@/composables/compat'
+import { usePageContext } from '@/renderer/usePageContext'
 
 export default defineComponent({
   props: {
@@ -14,13 +21,28 @@ export default defineComponent({
       type: [String, Object] as PropType<Route>,
       required: true,
     },
+    activeClass: {
+      type: String,
+      default: 'active',
+    },
+    exactActiveClass: {
+      type: String,
+      default: 'exact-active',
+    },
   },
   setup(props) {
-    const { routeToPath } = useRouter()
+    const pageContext = usePageContext()
+    const { routeToPath, localePath } = useRouter()
     const href = computed(() => routeToPath(props.to))
+    const hrefPathname = computed(() => href.value.split(/[?#]/)[0])
+    const currentPathname = computed(() => localePath(pageContext.urlOriginal))
+    const active = computed(() => currentPathname.value.startsWith(hrefPathname.value))
+    const exactActive = computed(() => currentPathname.value == hrefPathname.value)
 
     return {
       href,
+      active,
+      exactActive,
     }
   },
 })
