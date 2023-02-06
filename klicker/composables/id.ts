5cfd695c17090edcab920ede1e656f4de7f38a6c
schneefux <schneefux+commit@schneefux.xyz>
2023-02-06 10:05:34 +0100

    klicker: Fix unique ID generation

--- klicker/composables/id.ts
+++ klicker/composables/id.ts
@@ -1,14 +1,12 @@
-import { ref, nextTick, onMounted, getCurrentInstance } from 'vue'
+import { ref, getCurrentInstance } from 'vue'
 
 export const useUniqueId = () => {
   const id = ref()
-  const instance = getCurrentInstance() as any
-  onMounted(() => nextTick(() => {
-    if (instance != null) {
-      const uid = instance?.uid ?? instance?.proxy?._uid
-      id.value = `__KLICKERID__${uid}`
-    }
-  }))
+  const instance = getCurrentInstance()
+  if (instance != null) {
+    const uid = instance?.uid
+    id.value = `klicker-${uid}`
+  }
 
   return {
     id,
