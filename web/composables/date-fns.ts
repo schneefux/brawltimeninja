5b238c1f27e0d66e24e1637f59107d37c2e81236
schneefux <schneefux+commit@schneefux.xyz>
2023-02-07 09:51:02 +0100

    web: Fix date-fns TypeError

--- web/composables/date-fns.ts
+++ web/composables/date-fns.ts
@@ -1,12 +1,12 @@
 import { onMounted, ref } from 'vue'
 import { useI18n } from 'vue-i18n'
-import { LocaleCode } from '~/locales'
+import { LocaleIso } from '~/locales'
 
-const localeImportMap: Record<LocaleCode, any> = {
+const localeImportMap: Record<LocaleIso, any> = {
   en: () => import('date-fns/locale/en-US/index'),
   de: () => import('date-fns/locale/de/index'),
   es: () => import('date-fns/locale/es/index'),
-  ua: () => import('date-fns/locale/uk/index'),
+  uk: () => import('date-fns/locale/uk/index'),
   it: () => import('date-fns/locale/it/index'),
   ru: () => import('date-fns/locale/ru/index'),
 }
@@ -14,7 +14,11 @@ const localeImportMap: Record<LocaleCode, any> = {
 export const useDateFnLocale = () => {
   const i18n = useI18n()
   const locale = ref()
-  onMounted(async () => locale.value = (await localeImportMap[i18n.locale.value as LocaleCode]())?.default)
+  onMounted(async () => {
+    if (i18n.locale.value in localeImportMap) {
+      locale.value = (await localeImportMap[i18n.locale.value as LocaleIso]()).default
+    }
+  })
 
   return {
     locale,
