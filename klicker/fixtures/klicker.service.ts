a9e3da20fb4a00ab47e03ba70f328eac4ce0d273
schneefux <schneefux+commit@schneefux.xyz>
2023-02-08 15:03:51 +0100

    klicker: Fix types

--- klicker/fixtures/klicker.service.ts
+++ klicker/fixtures/klicker.service.ts
@@ -14,7 +14,7 @@ export function translate(key: string) {
 
 export class KlickerServiceMock extends KlickerService {
   constructor() {
-    super('https://cube.brawltime.ninja', config, [], [], [], [], [])
+    super('https://cube.brawltime.ninja', config, [], [], [], [], [], window.fetch.bind(window))
   }
 }
 
