35fd1d7d7830ec02a5606f498e076e1e3cf45ff8
schneefux <schneefux+commit@schneefux.xyz>
2023-11-08 08:33:52 +0100

    media: Use debian+jemalloc

--- deployment/jobs/deploy.sh
+++ deployment/jobs/deploy.sh
@@ -2,4 +2,4 @@
 FILES=${1:-$(find . -name "brawltime-*.nomad")}
 COMMIT=${2:-$(git rev-parse HEAD)}
 HASH=$(echo $COMMIT | cut -c1-7)
-echo "$FILES" | xargs -I% nomad run -hcl2-strict=false -preserve-counts -var-file vars.nomadvars -var="tag=sha-$HASH" %
+echo "$FILES" | xargs -I% ./nomad run -hcl2-strict=false -preserve-counts -var-file vars.nomadvars -var="tag=sha-$HASH" %
