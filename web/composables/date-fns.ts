d2a29fcd30381982c82b7a55602d0373f56d515a
schneefux <schneefux+commit@schneefux.xyz>
2023-12-31 09:58:26 +0100

    web: Fix date-fn caused hydration mismatch

--- web/composables/date-fns.ts
+++ web/composables/date-fns.ts
@@ -1,26 +1,16 @@
-import { onMounted, ref } from 'vue'
+import { computed } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { LocaleIso } from '~/locales'
 import { Locale } from 'date-fns'
+import { enUS, de, es, uk, it, ru, pl } from 'date-fns/locale'
 
-const localeImportMap: Record<LocaleIso, () => Promise<Locale>> = {
-  en: () => import('date-fns/locale/en-US').then(i => i['enUS']),
-  de: () => import('date-fns/locale/de').then(i => i['de']),
-  es: () => import('date-fns/locale/es').then(i => i['es']),
-  uk: () => import('date-fns/locale/uk').then(i => i['uk']),
-  it: () => import('date-fns/locale/it').then(i => i['it']),
-  ru: () => import('date-fns/locale/ru').then(i => i['ru']),
-  pl: () => import('date-fns/locale/pl').then(i => i['pl']),
-}
+// unfortunately, these cannot be lazy-loaded,
+// they have to be available immediately as to not cause hydration mismatches
+const localeImportMap: Record<LocaleIso, Locale> = { en: enUS, de, es, uk, it, ru, pl, }
 
 export const useDateFnLocale = () => {
   const i18n = useI18n()
-  const locale = ref<Locale>()
-  onMounted(async () => {
-    if (i18n.locale.value in localeImportMap) {
-      locale.value = (await localeImportMap[i18n.locale.value as LocaleIso]())
-    }
-  })
+  const locale = computed(() => localeImportMap[i18n.locale.value as LocaleIso])
 
   return {
     locale,
