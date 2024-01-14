eb16af47f2b3424ee24d5e10dc1d751bb12d9058
schneefux <schneefux+commit@schneefux.xyz>
2024-01-14 18:57:23 +0100

    web: Update router bug workaround

--- web/composables/route-params.ts
+++ web/composables/route-params.ts
@@ -2,14 +2,21 @@ import { ref, watch } from 'vue'
 import { useRoute, RouteParams } from 'vue-router';
 
 /**
- * Workaround for vue bug where components rerender upon leaving the route
+ * Workaround for Vue bug where components rerender upon leaving the route
  * see https://github.com/vuejs/vue-router/issues/3393#issuecomment-1158470149
 */
 export const useRouteParams = () => {
   const route = useRoute()
 
+  const validRoutePath = route.path
   const params = ref<RouteParams>()
-  watch(() => route.params, newParams => params.value = newParams, {
+  watch(() => route.params, newParams => {
+    if (route.path != validRoutePath) {
+      console.warn('Route params changed before component was unmounted, skipping param update')
+      return
+    }
+    params.value = newParams
+  }, {
     immediate: true,
     flush: 'post',
   })
