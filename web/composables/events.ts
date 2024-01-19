c6023201d42d9d4bd70abe43f07ec27ef669a604
schneefux <schneefux+commit@schneefux.xyz>
2024-01-19 15:04:00 +0100

    web: Fix dead links due to event name mismatch

--- web/composables/events.ts
+++ web/composables/events.ts
@@ -3,33 +3,51 @@ import { ActiveEvent } from "~/model/Api"
 import { EventMetadata } from "~/plugins/klicker.service"
 import { computed } from "vue"
 import { useApi, useAsync } from "./compat"
+import { useAllEvents } from "./dimension-values"
 
 export function useCurrentAndUpcomingEvents() {
   const $api = useApi()
 
   const events = useAsync(() => $api.events.active.query().catch(() => ({ current: [], upcoming: [] })), 'events')
+  const allEvents = useAllEvents()
 
-  const current = computed<(ActiveEvent & EventMetadata)[]>(() => (events.value?.current ?? []).map(e => ({
-    id: e.id,
-    map: e.map,
-    mode: unformatMode(e.mode),
-    key: `${e.id}-${e.mode}-${e.map}`,
-    metrics: {},
-    powerplay: false,
-    start: e.start,
-    end: e.end,
-  })).filter((event, index, all) => all.findIndex(e => e.key == event.key) == index))
+  // FIXME simplify this code when the official API returns future events
+  // Brawlify's map name formatting differs slightly, overwrite them
+  // for example: "Belle's Rock" => "Belles Rock", "Waters of Doom" => "Waters Of Doom"
+  const fixName = (event: ActiveEvent & EventMetadata): ActiveEvent & EventMetadata => ({
+    ...event,
+    ...(allEvents.value.find(e => event.id == e.id) ?? {}),
+  })
 
-  const upcoming = computed<(ActiveEvent & EventMetadata)[]>(() => (events.value?.upcoming ?? []).map(e => ({
-    id: e.id,
-    map: e.map,
-    mode: unformatMode(e.mode),
-    key: `${e.id}-${e.mode}-${e.map}`,
-    metrics: {},
-    powerplay: false,
-    start: e.start,
-    end: e.end,
-  })).filter((event, index, all) => all.findIndex(e => e.key == event.key) == index))
+  const current = computed<(ActiveEvent & EventMetadata)[]>(() => (events.value?.current ?? [])
+    .map(e => ({
+      id: e.id,
+      map: e.map,
+      mode: unformatMode(e.mode),
+      key: `${e.id}-${e.mode}-${e.map}`,
+      metrics: {},
+      powerplay: false,
+      start: e.start,
+      end: e.end,
+    }))
+    .filter((event, index, all) => all.findIndex(e => e.key == event.key) == index)
+    .map(e => fixName(e))
+  )
+
+  const upcoming = computed<(ActiveEvent & EventMetadata)[]>(() => (events.value?.upcoming ?? [])
+    .map(e => ({
+      id: e.id,
+      map: e.map,
+      mode: unformatMode(e.mode),
+      key: `${e.id}-${e.mode}-${e.map}`,
+      metrics: {},
+      powerplay: false,
+      start: e.start,
+      end: e.end,
+    }))
+    .filter((event, index, all) => all.findIndex(e => e.key == event.key) == index)
+    .map(e => fixName(e))
+  )
 
   return {
     current,
