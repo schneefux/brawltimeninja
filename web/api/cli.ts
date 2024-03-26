c17505c61befba87bfafdd8d0ff077b08f70e4ff
schneefux <schneefux+commit@schneefux.xyz>
2024-03-26 16:02:38 +0100

    web: Replace cron endpoint by CLI

---
+++ web/api/cli.ts
@@ -0,0 +1,42 @@
+import 'dotenv/config'
+import { Command } from 'commander'
+import { updateAllProfiles } from './routes/player'
+import { updateAllReports, updateReport } from './routes/report'
+
+const program = new Command()
+
+program
+  .command('update-profiles')
+  .description('Update tracked profiles')
+  .action(async () => {
+    console.time('profiles update')
+    const summary = await updateAllProfiles()
+    console.timeEnd('profiles update')
+    console.log(summary)
+  })
+
+program
+  .command('update-reports')
+  .description('Update AI reports')
+  .action(async () => {
+    console.time('reports update')
+    const summary = await updateAllReports()
+    console.timeEnd('reports update');
+    console.log(summary)
+  })
+
+program
+  .command('update-report')
+  .description('Update a report')
+  .option('-l, --locale <locale>', 'Locale')
+  .option('-m, --mode <mode>', 'Mode')
+  .option('-p, --map <map>', 'Map')
+  .action(async (options) => {
+    console.time('report update')
+    const { locale, mode, map } = options
+    const summary = await updateReport(locale, mode, map)
+    console.timeEnd('report update')
+    console.log(summary)
+  })
+
+program.parse(process.argv)
--- web/api/index.ts
+++ web/api/index.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import { createExpressMiddleware } from '@trpc/server/adapters/express'
-import { playerRouter, updateAllProfiles } from './routes/player'
+import { playerRouter } from './routes/player'
 import { clubRouter } from './routes/club'
 import { createContext } from './context'
 import { rankingsRouter } from './routes/rankings'
@@ -9,7 +9,7 @@ import { router } from './trpc'
 import renderRouter from './routes/render'
 import klickerRouter from './routes/klicker'
 import etag from 'etag'
-import { reportRouter, updateAllReports, updateReport } from './routes/report'
+import { reportRouter } from './routes/report'
 
 const appRouter = router({
   player: playerRouter,
@@ -22,46 +22,6 @@ const appRouter = router({
 export type AppRouter = typeof appRouter
 
 const app = express()
-
-// TODO restrict to localhost
-app.post('/cron', async (req, res, next) => {
-  console.time('running cron jobs')
-  try {
-    const summary = await updateAllProfiles()
-    res.json(summary)
-  } catch (err) {
-    console.error(err)
-    next(err)
-  }
-  console.timeEnd('running cron jobs')
-})
-
-// TODO move to cron
-app.post('/update-reports', async (req, res, next) => {
-  console.time('reports update')
-  try {
-    const summary = await updateAllReports()
-    res.json(summary)
-  } catch (err) {
-    console.error(err)
-    next(err)
-  }
-  console.timeEnd('reports update')
-})
-
-// TODO remove, only for testing
-app.post('/update-report', express.json(), async (req, res, next) => {
-  console.time('report update')
-  try {
-    const { locale, mode, map } = req.body
-    const summary = await updateReport(locale as string, mode as string, map as string)
-    res.json(summary)
-  } catch (err) {
-    console.error(err)
-    next(err)
-  }
-  console.timeEnd('report update')
-})
 
 app.use('/render', renderRouter)
 app.use('/klicker', klickerRouter)
