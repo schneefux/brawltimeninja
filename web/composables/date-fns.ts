222b90d1e8be81786bb6bfc10a7506af135463cb
schneefux <schneefux+commit@schneefux.xyz>
2025-05-14 13:24:00 +0200

    web: Fix date hydration error

--- web/composables/date-fns.ts
+++ web/composables/date-fns.ts
@@ -3,6 +3,7 @@ import { useI18n } from 'vue-i18n'
 import { LocaleIso } from '~/locales'
 import { Locale, format, formatDistanceToNow } from 'date-fns'
 import { enUS, de, es, uk, it, ru, pl, bn, fi, fr, hi, ja, lt, nl, pt, sk, sv, tr, vi, zhCN, ptBR, cs, ko } from 'date-fns/locale'
+import { ssrRef } from './compat'
 
 // unfortunately, these cannot be lazy-loaded,
 // they have to be available immediately as to not cause hydration mismatches
@@ -46,7 +47,7 @@ export const useDateFnLocale = () => {
 export const useFormattedDistanceToNow = (timestamp: MaybeRef<Date|undefined>, { addSuffix }: { addSuffix: MaybeRef<boolean> }) => {
   const { locale } = useDateFnLocale()
 
-  const formatted = ref('…')
+  const formatted = ssrRef('…', computed(() => `timediff-${unref(timestamp)}`))
 
   const updateFormatted = () => {
     const ts = unref(timestamp)
@@ -71,14 +72,14 @@ export const useFormattedDistanceToNow = (timestamp: MaybeRef<Date|undefined>, {
 export const useFormattedDate = (timestamp: MaybeRef<Date>, formatStr: MaybeRef<string>) => {
   const { locale } = useDateFnLocale()
 
-  const formatted = ref('…')
+  const formatted = ssrRef('…', computed(() => `timestamp-${unref(timestamp)}-${unref(formatStr)}`))
 
   const updateFormatted = () => {
     formatted.value = format(unref(timestamp), unref(formatStr), {
       locale: locale.value,
     })
   }
-  watch(() => [timestamp, locale], updateFormatted, {
+  watch(() => [timestamp, formatStr, locale], updateFormatted, {
     immediate: true,
   })
 
